1a4cc5ed156f18cba930b12d97f5d242b86f9a21
Mateusz_K <mateusz.kedzior@millwardbrown.com>
2016-02-12 17:56:58 +0100

    Pierwsza wersja.

---
+++ SprawdzanieNIPu/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NIP przyklad = new NIP("1744751686");
+            string jaka = przyklad.czyPoprawna ? "jest prawidłowy": "jest niepoprawny";
+
+            Console.Write(string.Format("Numer NIP: {0} {1}", przyklad.podziel, jaka));
+			
+			NIP inny = new NIP("1744751684");
+            jaka = inny.czyPoprawna ? "jest prawidłowy": "jest niepoprawny";
+
+            Console.Write(string.Format("Numer NIP: {0} {1}", inny.podziel, jaka));
+
+            var name = Console.ReadLine();
+        }
+    }
+}
