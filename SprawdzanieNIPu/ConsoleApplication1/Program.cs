80e3991cdf6b1260d2ded2a28808f5db665f206e
Mateusz_K <mateusz.kedzior@millwardbrown.com>
2016-02-12 10:28:38 +0100

    Program do weryfikacji numeru NIP dodany (C#, Monodevelop).

---
+++ SprawdzanieNIPu/ConsoleApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NIP mk = new NIP("5342348938");
+            string jaka = mk.czyPoprawna ? "jest prawidłowy": "jest niepoprawny";
+
+            Console.Write(string.Format("Numer NIP: {0} {1}", mk.podziel, jaka));
+
+            var name = Console.ReadLine();
+        }
+    }
+}
